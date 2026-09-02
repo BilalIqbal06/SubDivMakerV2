185b4691f8a8cfdfc0acb4c312cc2417f90a800d
Bilal Iqbal <bilaliqbal@vt.edu>
2026-09-02 08:22:59 -0400

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/SubDivMakerV2/' : '/',
   plugins: [react()],
   server: {
     port: 3004,
@@ -25,4 +26,4 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: true
   }
-})
+}))
